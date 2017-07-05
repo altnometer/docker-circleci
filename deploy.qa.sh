e0ab5c4537c2ef703a4b6c8c9d62ea8d96acaeb1
sam <sam@redmoo.info>
2017-07-05 11:11:17 +0300

    Corrected to docker tags to use 'redmoo' user.

--- deploy.qa.sh
+++ deploy.qa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push willrstern/sample-node:qa
+docker push redmoo/sample-node:qa
 
-ssh deploy@107.170.19.20 << EOF
-docker pull willrstern/sample-node:qa
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /home/sam/.ssh/circleci deploy@46.101.104.66 << EOF
+docker pull redmoo/sample-node:qa
 docker stop web || true
 docker rm web || true
-docker rmi willrstern/sample-node:current || true
-docker tag willrstern/sample-node:qa willrstern/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 willrstern/sample-node:current
+docker rmi redmoo/sample-node:current || true
+docker tag redmoo/sample-node:qa redmoo/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 redmoo/sample-node:current
 EOF
--- deploy.staging.sh
+++ deploy.staging.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push willrstern/sample-node:staging
+docker push redmoo/sample-node:staging
 
-ssh deploy@107.170.19.19 << EOF
-docker pull willrstern/sample-node:staging
+ssh deploy@139.59.138.25 << EOF
+docker pull redmoo/sample-node:staging
 docker stop web || true
 docker rm web || true
-docker rmi willrstern/sample-node:current || true
-docker tag willrstern/sample-node:staging willrstern/sample-node:current
-docker run -d --net app --restart always --name web -p 3000:3000 willrstern/sample-node:current
+docker rmi redmoo/sample-node:current || true
+docker tag redmoo/sample-node:staging redmoo/sample-node:current
+docker run -d --net app --restart always --name web -p 3000:3000 redmoo/sample-node:current
 EOF
